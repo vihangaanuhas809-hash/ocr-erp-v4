e7b24eceea55a88d29b7305b700ae4007eeed6d0
vihangaanuhas809-hash <vihangaanuhas809@gmail.com>
2026-07-17 09:52:55 +0530

    Update route.ts

--- app/api/materials/route.ts
+++ app/api/materials/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET() {
-  const materials = await prisma.Material.findMany({
+  const materials = await prisma.material.findMany({
     include: {
       project: true,
     },
@@ -17,7 +17,7 @@ export async function GET() {
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  const material = await prisma.Material.create({
+  const material = await prisma.material.create({
     data: {
       name: body.name,
       unit: body.unit,
